1e647fa9c35c4f3015a1f9754ec98d9a440c76ba
AndreaCrisa <andreacrisa.23@gmail.com>
2024-12-02 16:37:27 +0100

    Updated disk jockey

---
+++ versions/diskJockey.js
@@ -0,0 +1,116 @@
+// Helper to create a chain for each element
+const createChannelWithEffects = (volume = -6) => {
+    const gain = new Tone.Volume(volume);
+    const filter = new Tone.Filter(20000, "lowpass");
+    const reverb = new Tone.Reverb({ decay: 3, wet: 0.5 });
+    gain.chain(filter, reverb, Tone.Destination);
+    return { gain, filter, reverb };
+};
+
+// Elements with individual effects
+const kickChannel = createChannelWithEffects();
+const kick = new Tone.Player("../DrumSamples/BD.wav").connect(kickChannel.gain);
+
+const snareChannel = createChannelWithEffects();
+const snare = new Tone.Player("../DrumSamples/SNARE.wav").connect(snareChannel.gain);
+
+const hiHatChannel = createChannelWithEffects();
+const ClosedHiHat = new Tone.Player("../DrumSamples/CHH.wav").connect(hiHatChannel.gain);
+
+const bassChannel = createChannelWithEffects();
+const bassSynth = new Tone.AMSynth().connect(bassChannel.gain);
+
+const arpChannel = createChannelWithEffects();
+const arpSynth = new Tone.PolySynth(Tone.Synth).connect(arpChannel.gain);
+
+const riffChannel = createChannelWithEffects();
+const mainRiffSynth = new Tone.PolySynth(Tone.Synth).connect(riffChannel.gain);
+
+// Drum pattern
+const drumPatternIntro = new Tone.Sequence((time, note) => {
+    if (document.getElementById("toggleKick").checked && note === "kick") {
+        kick.start(time);
+    }
+    if (document.getElementById("toggleSnare").checked && note === "snare") {
+        snare.start(time);
+    }
+    if (document.getElementById("toggleHiHat").checked && note === "ClosedHiHat") {
+        ClosedHiHat.start(time);
+    }
+}, ["kick", "ClosedHiHat", "snare", "ClosedHiHat"], "8n");
+
+// Bassline
+const bassLine = new Tone.Sequence((time, note) => {
+    if (document.getElementById("toggleBass").checked) {
+        bassSynth.triggerAttackRelease(note, "8n", time);
+    }
+}, ["G2", "F2", "Eb2", "Eb2", "Eb2", "F2", "C2", "C2", "C2", "D2", "Eb2", "Eb2", "Eb2", "F2", "G2", "G2"], "4n");
+
+// Arpeggio
+const arpSequence = new Tone.Sequence((time, note) => {
+    if (document.getElementById("toggleArp").checked) {
+        arpSynth.triggerAttackRelease(note, "16n", time);
+    }
+}, ["C4", "D4", "F4", "C4", "D4", "F4",
+    "C4", "D4", "G4",
+    "C4", "D4", "F4", "C4", "D4", "F4", "C4", "D4", "F4", "C4", "D4", "F4",
+    "C4", "D4", "G4",
+    "C4", "D4", "F4", "C4", "D4", "F4", "C4", "D4", "F4", "C4", "D4", "F4", "C4", "D4", "F4",
+    "C4", "D4", "G4",
+    "C4", "D4", "F4", "C4", "D4", "F4",
+    "C4", "D4", "Bb4", "C5",
+    "C4", "D4", "G4",
+    "C4", "D4", "Bb4",
+    "C4", "D4", "G4",
+    "C4", "D4", "F4"], "16n");
+
+// Main riff
+const mainRiffL = new Tone.Sequence((time, note) => {
+    if (document.getElementById("toggleRiff").checked) {
+        mainRiffSynth.triggerAttackRelease(note, "16n", time);
+    }
+}, ["G4", null, "G4", null, "F4", null, "F4", null, "D4", "D4", null, "D4", null, "D4", null, null], "16n");
+
+const mainRiffH = new Tone.Sequence((time, note) => {
+    if (document.getElementById("toggleRiff").checked) {
+        mainRiffSynth.triggerAttackRelease(note, "16n", time);
+    }
+}, ["Bb4", null, "Bb4", null, "A4", null, "A4", null, "G4", "G4", null, "G4", null, "G4", null, null], "16n");
+
+
+// Update controls
+function updateControl(channel, volumeId, reverbId, filterId) {
+    document.getElementById(volumeId).addEventListener("input", (e) => {
+        channel.gain.volume.value = e.target.value;
+    });
+    document.getElementById(reverbId).addEventListener("input", (e) => {
+        channel.reverb.wet.value = e.target.value;
+    });
+    document.getElementById(filterId).addEventListener("input", (e) => {
+        channel.filter.frequency.value = e.target.value;
+    });
+}
+
+// Attach controls
+updateControl(kickChannel, "kickVolume", "kickReverb", "kickFilter");
+updateControl(snareChannel, "snareVolume", "snareReverb", "snareFilter");
+updateControl(hiHatChannel, "hiHatVolume", "hiHatReverb", "hiHatFilter");
+updateControl(bassChannel, "bassVolume", "bassReverb", "bassFilter");
+updateControl(arpChannel, "arpVolume", "arpReverb", "arpFilter");
+updateControl(riffChannel, "riffVolume", "riffReverb", "riffFilter");
+
+// Start/Stop buttons
+document.getElementById("playButton").addEventListener("click", () => {
+    Tone.start();
+    drumPatternIntro.start(0);
+    bassLine.start(0);
+    arpSequence.start(0);
+    mainRiffL.start(0);
+    mainRiffH.start(0);
+    Tone.Transport.start();
+});
+
+document.getElementById("stopButton").addEventListener("click", () => {
+    Tone.Transport.stop();
+    Tone.Transport.cancel();
+});
